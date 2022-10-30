081e64b8fa7200f1eaecfd3cb486939368452a75
Weslley Barboza <weslleybarboza@gmail.com>
2022-10-30 08:34:38 +0000

    desenv

--- interact_s3.py
+++ interact_s3.py
@@ -4,7 +4,11 @@ import pandas as pd
 
 s3_cliente = boto3.client('s3')
 
+s3_cliente.upload_file("data/MICRODADOS_ENEM_2020.csv",
+                        "datalake-weslley-156814630337",
+                        "data-raw/enem/year=2020/MICRODADOS_ENEM_2020.csv")
+
 s3_cliente.upload_file("data/MICRODADOS_ENEM_2021.csv",
                         "datalake-weslley-156814630337",
-                        "data-raw/MICRODADOS_ENEM_2021.csv")
+                        "data-raw/enem/year=2021/MICRODADOS_ENEM_2021.csv")
 
