0f4db661f8510b4b08ce51b00167702a07d4b990
weslleybarboza <weslleybarboza@gmail.com>
2022-10-30 13:58:50 +0100

    removido path das variaveis

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 #centralizar o arquivo de controle de estado do terraform
 terraform{
   backend "s3" {
-    bucket = "${var.base_bucket_name}-${var.environment}-${var.account_number}-terraform-state"
+    bucket = "datalake-weslley-production-156814630337-terraform-state"
     key = "state/terraform.tfstate"
     region = var.region
   }
