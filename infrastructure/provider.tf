45d6202ba0705bb3e815efca9602da92519b7464
weslleybarboza <weslleybarboza@gmail.com>
2022-10-30 13:29:58 +0100

    alteracao de pasta arq terraform para infrastructure

---
+++ infrastructure/provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+#centralizar o arquivo de controle de estado do terraform
+terraform{
+  backend "s3" {
+    bucket = "datalake-weslley-production-156814630337-terraform-state"
+    key = "state/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,16 @@
+variable "base_bucket_name" {
+  default = "datalake-weslley"
+}
+
+variable "environment" {
+  default = "production"
+}
+
+variable "account_number" {
+  default = "156814630337"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
+
