98293cd25a2be5e2ad1e4580c40df61f5ef5f90b
weslleybarboza <weslleybarboza@gmail.com>
2022-11-02 14:39:58 +0100

    inclui servicos kinesis

---
+++ infrastructure/kinesis.tf
@@ -0,0 +1,45 @@
+#knesis - ferramenta para entrega de dados - micro batchs
+
+resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
+  name        = "weslley-kinesis-firehose-s3-stream"
+  destination = "extended_s3"
+
+  extended_s3_configuration {
+    role_arn   = aws_iam_role.firehose_role.arn
+    bucket_arn = aws_s3_bucket.stream.arn
+    prefix = "firehose/" #por padrao o kiness entrega os dados por dia mes ano, porem podemos add uma pasta para agrupar
+    buffer_size = 5 #a cada 5mb faz entrega ou
+    buffer_interval = 60 #a cada 60 segundos
+    
+    cloudwatch_logging_options {
+        enabled = true
+        log_group_name = aws_cloudwatch_log_group.firehose.name
+        log_stream_name = aws_cloudwatch_log_stream.firehose.name
+    }
+    
+  }
+    tags = {
+    cliente   = "weslley"
+  }
+}
+
+
+resource "aws_iam_role" "firehose_role" {
+  name = "weslley_firehose_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "firehose.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,6 +1,26 @@
+#bucket utilizado para armazenar os dados processados
+
 resource "aws_s3_bucket" "datalake" {
   #parametros de config do recurso escolhido
   bucket = "${var.base_bucket_name}-${var.environment}-${var.account_number}-tf"
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    cliente = "weslley"
+  }
+}
+
+#bucket utilizado para dados stream
+resource "aws_s3_bucket" "stream" {
+  #parametros de config do recurso escolhido
+  bucket = "${var.base_bucket_name}-${var.environment}-${var.account_number}-stream-tf"
   acl    = "private"
   server_side_encryption_configuration {
     rule {
